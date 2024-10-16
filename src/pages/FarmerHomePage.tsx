2002caadae6677cb763a69d8a1c14e7e01f11648
Hirushat <hirusha10online@gmail.com>
2024-10-17 00:40:08 +0530

    created MyStock.tsx

--- src/pages/FarmerHomePage.tsx
+++ src/pages/FarmerHomePage.tsx
@@ -5,7 +5,7 @@ import SelectOption from '../components/homePage/SelectOption'
 
 import MillersBg from '../assets/millersBg.png'
 import BuyersBg from '../assets/buyersBg.png'
-import PriceListBg from '../assets/priceBg.png'
+import MyStockBg from '../assets/myStock.png'
 
 
 const FarmerHomePage = () => {
@@ -13,7 +13,7 @@ const FarmerHomePage = () => {
     <div className='bg-gradient-to-b from-[#E5AD2A] via-[#CC8A2E] to-[#B2A72A]'>
       <NavBar topic="Home"/>
       <Banner/>
-      <SelectOption img1={MillersBg} img2={BuyersBg} img3={PriceListBg} topic1="Miller" topic2="Buyer" topic3="Add Stock"/>
+      <SelectOption img1={MillersBg} img2={BuyersBg} img3={MyStockBg} topic1="Miller" topic2="Buyer" topic3="My Stock"/>
     </div>
   )
 }
